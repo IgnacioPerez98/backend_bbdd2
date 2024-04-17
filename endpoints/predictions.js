bd65109d69dea24dcc063c0ab800e8dfd3194e01
NicorRodriguez <poptnico75@gmail.com>
2024-04-17 20:10:34 -0300

    Soy un crack

--- endpoints/predictions.js
+++ endpoints/predictions.js
@@ -1,28 +1,21 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const handlerPredictions = require('../handlers/handlerPredictions')
-
+const handlerPredictions = require('../handlers/handlerPredictions');
 
 //crud preditions
 
-router.post('/prediction', (req, res, next) =>{
+router.post('/', async (req, res, next) => {
+  return res.sendStatus(200);
+});
+router.patch('/', (req, res, next) => {
+  //controlar que sea una hora antes del partido
+});
 
-})
-router.patch('/prediction', (req, res, next) =>{
-    //controlar que sea una hora antes del partido
+router.get('/', async (req, res) => {
+  console.log('eeaaa');
+  return res.json({ tu: 'ki' });
+});
 
-})
-
-router.get('/prediction', (req, res,next)=> {
-
-})
-
-
-router.delete('/predition', (req,res,next) => {
-
-})
-
-
-
+router.delete('/', (req, res, next) => {});
 
 module.exports = router;
