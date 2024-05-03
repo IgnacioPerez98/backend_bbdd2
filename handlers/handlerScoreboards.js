11e5a09a6e3114d602789757b23c8f6e85052154
PTechSoftware <ptechsoftware.correo@gmail.com>
2024-05-03 19:03:11 -0300

    controlador de partidos

---
+++ handlers/handlerScoreboards.js
@@ -0,0 +1,9 @@
+const PostgresService = require('../services/PostgresService')
+const errors = require('../services/errorsmessages')
+
+
+let handlerScoreBoards = {
+    
+
+}
+module.exports = handlerScoreBoards;
